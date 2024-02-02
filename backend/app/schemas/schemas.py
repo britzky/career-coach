48f3f91a8a8a89d8da39ccd7b9e577520a9ab00d
John <johnclapper89@gmail.com>
2024-02-02 18:42:42 -0500

    Added schemas for user and roadmap

--- backend/app/schemas/schemas.py
+++ backend/app/schemas/schemas.py
@@ -12,4 +12,36 @@ class CareerInfo(BaseModel):
     timeframe: str
     current_knowledge: str
 
-# Schema for User 
+# Schema for User
+class User(BaseModel):
+    username: str
+    email: str
+    password: str
+
+# Schema for roadmap course info
+class RoadmapCourseInfo(BaseModel):
+    coursename: str
+    link: str
+    skilllevel: str
+    price: str
+    description: str
+
+# Schema for roadmap sections
+class RoadmapSection(BaseModel):
+    month: str
+    title: str
+    courseInfo: RoadmapCourseInfo
+
+# Schema for creating the roadmap object
+class RoadmapObject(BaseModel):
+    summary: str
+    roadmap: List[RoadmapSection]
+
+# Schema for reading the roadmap from the database
+class Roadmap(RoadmapObject):
+    id: int
+    user_id: int
+
+    # Class to tell Pydantic to treat the models as dictionaries
+    class Config:
+        orm_mode = True
