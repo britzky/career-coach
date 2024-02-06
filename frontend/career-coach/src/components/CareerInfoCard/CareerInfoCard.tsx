6ddc7e6cb1d8f1360a57bbc9b3eb1e4e5c7803d8
John <johnclapper89@gmail.com>
2024-02-05 19:39:53 -0500

    Started LearningStyle component

--- frontend/career-coach/src/components/CareerInfoCard/CareerInfoCard.tsx
+++ frontend/career-coach/src/components/CareerInfoCard/CareerInfoCard.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+interface CareerInfoCardProps {
+  text: string;
+}
 
-export const CareerInfoCard = () => {
+export const CareerInfoCard: React.FC<CareerInfoCardProps> = ({text}) => {
   return (
-    <div>CareerInfoCard</div>
+      <div className="border-2 border-purple-400 w-64 h-40 p-10 rounded-md">
+        <p className="text-purple-700">{text}</p>
+      </div>
   )
 }
--- frontend/career-coach/src/pages/LearningStyle/LearningStyle.tsx
+++ frontend/career-coach/src/pages/LearningStyle/LearningStyle.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { CareerInfoCard } from '../../components'
 
 export const LearningStyle = () => {
   return (
-    <div>LearningStyle</div>
+    <div className="flex justify-center min-h-screen w-full mt-14">
+      <div className="flex flex-col">
+        <div className="flex justify-center mb-8">
+          <p className="text-purple-700">What is preferred <span className="text-green-500">learning style</span>?</p>
+        </div>
+        <div className="flex gap-10 justify-center">
+          <div>
+            <CareerInfoCard text="Project Based" />
+          </div>
+          <div>
+            <CareerInfoCard text="Video Based" />
+          </div>
+          <div>
+            <CareerInfoCard text="Assessment Based" />
+          </div>
+          <div>
+            <CareerInfoCard text="Micro learning" />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
