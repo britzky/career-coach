25a9a393de18fdae25c738a33887a4efb1cdd7c5
juan-hernandez1 <juan.c.hernandez.312@gmail.com>
2024-02-05 18:36:46 -0600

    started building out tailwindcss config

--- frontend/career-coach/tailwind.config.js
+++ frontend/career-coach/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+
+      },
+      colors: {
+        ligthPurple: '#ECEEFB',
+      }
+    },
   },
   plugins: [],
 }
