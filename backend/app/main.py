b668b31c92ce87627c2ee2ddd5b927706422e208
John <johnclapper89@gmail.com>
2024-02-06 14:04:45 -0500

    Set up CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import roadmap, quote
 
 # initialize the app
 app = FastAPI()
+
+allowed_origins = [
+    "http://localhost:5173/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers =["*"]
+)
 
 app.include_router(roadmap.router)
 app.include_router(quote.router)
