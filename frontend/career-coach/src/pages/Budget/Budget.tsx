e23de919a633e419d7a4d12e0d76b8af115ab06a
juan-hernandez1 <juan.c.hernandez.312@gmail.com>
2024-02-07 11:24:28 -0600

    worked on budget, desired skills, and timeframe pages

--- frontend/career-coach/src/pages/Budget/Budget.tsx
+++ frontend/career-coach/src/pages/Budget/Budget.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { InputBox } from '../../components/InputBox'
-import { NavButton } from '../../components/NavButton';
+import { NavButton } from '../../components/NavButton'
 
 // export const Budget = () => {
 //   return (
@@ -22,16 +22,22 @@ const Footer: React.FC = ({ children }) => {
 
 export const Budget: React.FC = () => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', minHeight: '100vh' }}>
-      <div>
-        <p className='text-purple-900'>What is your budget? (optional)</p>
-      </div>
-      <div className='w-40'><InputBox placeholder='Enter your budget'/></div>
-      <div style={{ marginTop: 'auto' }}>
-        <Footer>
-          <NavButton>Back</NavButton>
-          <NavButton>Continue</NavButton>
-        </Footer>
+    <div className="flex justify-center min-h-screen w-full mt-14">
+      <div className="flex flex-col max-w-[1200px]">
+        <div className="flex justify-center mb-8">
+          <p className='text-purpleText text-base font-bold'>What is your <span className="bg-budget gradient-text">total budget</span>? (optional)</p>
+        </div>
+        <div className='flex justify-center'>
+          <div>
+            <InputBox placeholder='$100'/>
+          </div>
+        </div>
+        <div style={{ marginTop: 'auto' }}>
+          <Footer>
+            <NavButton>Back</NavButton>
+            <NavButton>Continue</NavButton>
+          </Footer>
+        </div>
       </div>
     </div>
   );
--- frontend/career-coach/src/pages/Timeframe/Timeframe.tsx
+++ frontend/career-coach/src/pages/Timeframe/Timeframe.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { InputBox } from '../../components/InputBox'
+import { NavButton } from '../../components/NavButton'
 
 export const Timeframe = () => {
   return (
-    <div>Timeframe</div>
+    <div className="flex justify-center min-h-screen w-full mt-14">
+      <div className="flex flex-col max-w-[1200px]">
+        <div className="flex justify-center mb-8">
+          <p className="text-purpleText text-base font-bold">What is your expected<span className="bg-timeframe gradient-text"> timeframe</span>?</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <div className='mb-6 w-40'>
+            <InputBox placeholder='12 Months'/>
+          </div>
+          <div className='mb-6 w-40'>
+            <InputBox placeholder='0 Years'/>
+          </div>         
+        </div>
+      </div>
+    </div>
   )
 }
