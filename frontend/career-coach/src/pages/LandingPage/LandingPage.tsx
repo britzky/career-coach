fda3885e097c10df0c5c103e84084bed5cab062b
John <johnclapper89@gmail.com>
2024-02-08 15:21:28 -0500

    Added button to landing page

--- frontend/career-coach/src/pages/LandingPage/LandingPage.tsx
+++ frontend/career-coach/src/pages/LandingPage/LandingPage.tsx
@@ -1,7 +1,15 @@
+import { useNavigate } from "react-router-dom";
 export const LandingPage = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1 className="font-bold text-xl bg-desired-job gradient-text max-w-[143px]">skills</h1>
+    <div className="flex justify-center items-center min-h-screen">
+      <button
+        type="button"
+        onClick={() => navigate('/desired-job')}
+        className="text-white bg-landing-page hover:bg-landing-page-reverse focus:ring-1 focus:outline-none focus:ring-purple-200 font-bold rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+      >
+        Try our product without an account!
+      </button>
     </div>
   )
 }
