404ce0969f6d1662c78d35763d1594503a0a9b47
John <johnclapper89@gmail.com>
2024-02-12 18:28:53 -0500

    Removed overview from landing page

--- frontend/career-coach/src/pages/LandingPage/LandingPage.tsx
+++ frontend/career-coach/src/pages/LandingPage/LandingPage.tsx
@@ -1,55 +1,19 @@
-import { useState } from "react"
 import { useNavigate } from "react-router-dom";
-import { Checkmark } from "../../assets/icons/Checkmark";
-
 
 export const LandingPage = () => {
-  const [selectedMonth, setSelectedMonth] = useState<number | undefined>(-1);
   const navigate = useNavigate();
-
-  const handleCircleClick = (monthIndex: number) => {
-    if (monthIndex === 0 && selectedMonth === 0) {
-      setSelectedMonth(-1)
-    } else {
-      setSelectedMonth(monthIndex)
-    }
-  }
 
   return (
     <div>
-      <div className="flex flex-col items-center mt-28">
-        <div className="border-2 border-purpleText py-2 px-10 rounded-lg">
-          <p className="text-xs font-bold mb-4 text-purpleText">Overview</p>
-          <div className="space-y-4">
-            {Array.from({ length: 12 }, (_, i) => (
-              <div key={i} className="flex items-center gap-16">
-                <span className="text-xs flex-1 font-medium">Month {i + 1}</span>
-                <div className="relative flex items-center">
-                  {i <= (selectedMonth ?? -1) ? (
-                    <div onClick={() => handleCircleClick(i)} className="w-6 h-6 bg-purple border-2 border-purpleText rounded-full flex items-center justify-center text-white z-10 cursor-pointer">
-                      <Checkmark />
-                    </div>
-                  ) : (
-                    <div onClick={() => handleCircleClick(i)} className="w-6 h-6 bg-buttonWhite border-2 border-purpleText rounded-full flex items-center justify-center text-white z-10 cursor-pointer"></div>
-                    )}
-                  {i < 11 && (
-                    <div className={`absolute w-1 h-6 top-full left-1/2 transform -translate-x-1/2 ${i <= (selectedMonth ?? -1) ? 'bg-purpleText': 'bg-line'}`}></div>
-                  )}
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
+      <div className="flex justify-center items-center min-h-screen">
+        <button
+          type="button"
+          onClick={() => navigate('/desired-job')}
+          className="text-white bg-landing-page hover:bg-landing-page-reverse focus:ring-1 focus:outline-none focus:ring-purple-200 font-bold rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        >
+          Try our product without an account!
+        </button>
       </div>
-    <div className="flex justify-center items-center min-h-screen">
-      <button
-        type="button"
-        onClick={() => navigate('/desired-job')}
-        className="text-white bg-landing-page hover:bg-landing-page-reverse focus:ring-1 focus:outline-none focus:ring-purple-200 font-bold rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-      >
-        Try our product without an account!
-      </button>
-    </div>
     </div>
   )
 }
