ad27bcd993aed32051c3902c66fd129efb72806f
juan-hernandez1 <juan.c.hernandez.312@gmail.com>
2024-02-12 13:50:11 -0600

    updated slider on hours per week page so that it synchronizes with input box

--- frontend/career-coach/src/components/InputBox/InputBox.tsx
+++ frontend/career-coach/src/components/InputBox/InputBox.tsx
@@ -1,12 +1,13 @@
 interface InputBoxProps {
     placeholder: string;
     border?: boolean;
+    value?: number | string; // added this line
     onChange?: (value: string) => void;
     onBlur?: (value: string) => void;
     required?: boolean;
 }
 
-export const InputBox: React.FC<InputBoxProps> = ({ placeholder, border=false, onChange, required=false, onBlur }) => {
+export const InputBox: React.FC<InputBoxProps> = ({ placeholder, border=false, value, onChange, required=false, onBlur }) => {
   const inputClass = border ? "w-full h-10 px-2 rounded-md border-2 border-purpleText" : "w-full h-10 px-2 rounded-md";
 
   return (
@@ -15,6 +16,7 @@ export const InputBox: React.FC<InputBoxProps> = ({ placeholder, border=false, o
           className={inputClass}
           type="text"
           placeholder={placeholder}
+          value={value} // added this line
           onChange={(e) => onChange && onChange(e.target.value)}
           onBlur={(e) => onBlur && onBlur(e.target.value)}
           required={required}
