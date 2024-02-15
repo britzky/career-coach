fbbcb452a726046cbc096af0e25bdb774ad340a7
John <johnclapper89@gmail.com>
2024-02-15 09:35:02 -0500

    Fixed overview component

--- frontend/career-coach/src/pages/Roadmap/Overview.tsx
+++ frontend/career-coach/src/pages/Roadmap/Overview.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react"
 import { Checkmark } from "../../assets/icons/Checkmark";
+import { useCareerDetails } from "../../context/CareerContext";
 
 export const Overview = () => {
     const [selectedMonth, setSelectedMonth] = useState<number>();
+    const { roadmap } = useCareerDetails()
 
     const handleCircleClick = (monthIndex: number) => {
         if (monthIndex === 0 && selectedMonth === 0) {
@@ -16,9 +18,9 @@ export const Overview = () => {
     <div className="border-2 border-purpleText py-2 px-10 rounded-lg">
       <p className="text-xs font-bold mb-4 text-purpleText">Overview</p>
       <div className="space-y-4">
-        {Array.from({ length: 12 }, (_, i) => (
+        {roadmap?.roadmap.map((item, i) => (
           <div key={i} className="flex items-center gap-16">
-            <span className="text-xs flex-1 font-medium">Month {i + 1}</span>
+            <span className="text-xs flex-1 font-medium">{item.month}</span>
             <div className="relative flex items-center">
               {i <= (selectedMonth ?? -1) ? (
                 <div onClick={() => handleCircleClick(i)} className="w-6 h-6 bg-purple border-2 border-purpleText rounded-full flex items-center justify-center text-white z-10 cursor-pointer">
@@ -27,7 +29,7 @@ export const Overview = () => {
               ) : (
                 <div onClick={() => handleCircleClick(i)} className="w-6 h-6 bg-buttonWhite border-2 border-purpleText rounded-full flex items-center justify-center text-white z-10 cursor-pointer"></div>
                 )}
-              {i < 11 && (
+              {i < roadmap?.roadmap.length - 1 && (
                 <div className={`absolute w-1 h-6 top-full left-1/2 transform -translate-x-1/2 ${i <= (selectedMonth ?? -1) ? 'bg-purpleText': 'bg-line'}`}></div>
               )}
             </div>
