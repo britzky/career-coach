34e2ffc3fc7b3839338072e713395e34716d9ed3
John <johnclapper89@gmail.com>
2024-02-15 17:53:13 -0500

    Added Footer

--- frontend/career-coach/src/components/Footer/Footer.tsx
+++ frontend/career-coach/src/components/Footer/Footer.tsx
@@ -1,13 +1,18 @@
+import facebook from '../../assets/images/facebook.png'
+import instagram from '../../assets/images/instagram.png'
+import x from '../../assets/images/x.png'
 
 export const Footer = () => {
   return (
     <footer className="flex items-center bg-footer h-[110px] w-full">
-        <div>
+        <div className="flex justify-between w-full mx-14 font-medium text-sm">
             <div>
                 <p>All rights reserved The Jays 2024</p>
             </div>
-            <div>
-                <img src="" alt="" />
+            <div className="flex gap-5">
+                <img src={facebook} alt="facebook" />
+                <img src={instagram} alt="instagram" />
+                <img src={x} alt="x" />
             </div>
         </div>
     </footer>
