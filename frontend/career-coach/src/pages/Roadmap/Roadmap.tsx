7aff4b1d74c7034a53409e587b59d99600d7473b
John <johnclapper89@gmail.com>
2024-02-15 09:08:04 -0500

    Adjusted layout of roadmap page

--- frontend/career-coach/src/pages/Roadmap/Roadmap.tsx
+++ frontend/career-coach/src/pages/Roadmap/Roadmap.tsx
@@ -5,21 +5,15 @@ import { Summary } from "./Summary";
 
 export const Roadmap = () => {
   return (
-    <div className="mt-28">
-      <div className="grid grid-cols-5 row-span-1">
-        <div className="col-span-1 ml-5">
-          <div className="flex flex-col gap-5">
-            <RoadmapDetails />
-            <Overview />
-          </div>
-        </div>
-        <div className="col-span-4">
-          <div className="flex flex-col gap-5 items-center">
-            <Summary />
-            <CourseAccordion />
-          </div>
+    <div className="mt-28 flex flex-col md:flex-row">
+      <div className="flex flex-col gap-5 ml-5 mb-5 md:mb-0 md:flex-1">
+        <RoadmapDetails />
+        <Overview />
+      </div>
+        <div className="flex flex-col gap-5 items-center flex-1 mx-7">
+          <Summary />
+          <CourseAccordion />
         </div>
       </div>
-    </div>
   );
 };
