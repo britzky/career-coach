40c3babfc19299d1cd2a3b70a1dcb0dc3aba607f
John <johnclapper89@gmail.com>
2024-02-19 16:48:57 -0500

    Adjusted road again

--- frontend/career-coach/src/pages/LandingPage/LandingPage.tsx
+++ frontend/career-coach/src/pages/LandingPage/LandingPage.tsx
@@ -11,14 +11,14 @@ export const LandingPage = () => {
   return (
     <div className="overflow-x-hidden flex flex-col min-h-screen">
       <div className="mt-28 mx-10">
-        <div className="flex flex-col mb-7 h-[795px]">
+        <div className="flex flex-col mb-7 h-[695px]">
+          <div className="relative mx-0 w-full">
+            <img src={road} alt="road" className="absolute -ml-28 mt-20 min-w-[1700px]" />
+          </div>
           <div className="max-w-[957px] flex flex-col">
             <span className="text-purpleText2 font-bold text-base">Get your DREAM JOB with our</span>
             <span className="gradient-text bg-landing-page-title font-bold text-xxl">AI-Powered Career Roadmaps</span>
             <p className="font-medium text-sm">Discover personalized courses, expert guidance, and a roadmap to your dream career, all powered by ChatGPT!</p>
-          </div>
-          <div className="relative">
-            <img src={road} alt="road" className="absolute" />
           </div>
           <div>
             <button
@@ -31,7 +31,7 @@ export const LandingPage = () => {
           </div>
         </div>
         <div className="overflow-x-hidden space-y-14 my-14">
-          <h1 className="font-bold text-xxl gradient-text bg-logo text-center">What is a Career Roadmap?</h1>
+          <h1 className="font-bold text-xxl gradient-text bg-logo text-center mt-14">What is a Career Roadmap?</h1>
           <p className="font-medium text-base">
             A career roadmap is your personalized guide to success,
             like a GPS for your career journey.
