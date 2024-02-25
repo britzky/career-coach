bc2cc6501e4830686bb564758868f1c896c91e81
John <johnclapper89@gmail.com>
2024-02-24 21:15:26 -0500

    Fixed your path to success layout

--- frontend/career-coach/src/pages/LandingPage/LandingPage.tsx
+++ frontend/career-coach/src/pages/LandingPage/LandingPage.tsx
@@ -13,7 +13,7 @@ export const LandingPage = () => {
   return (
     <div className="overflow-x-hidden flex flex-col min-h-screen">
       <div className="mt-28 mx-10">
-        <div className="flex flex-col mb-7 h-[695px]">
+        <div className="flex flex-col h-[695px]">
           <div className="relative mx-0 w-full">
             <img src={road} alt="road" className="absolute -ml-40 mt-20 min-w-[2750px]" />
           </div>
@@ -33,35 +33,37 @@ export const LandingPage = () => {
           </div>
         </div>
         <div className="overflow-x-hidden space-y-14 my-14 mt-[54rem]">
-          <h1 className="font-bold text-xxl gradient-text bg-logo text-center mt-14">What is a Career Roadmap?</h1>
-          <p className="font-medium text-base">
-            A career roadmap is your personalized guide to success,
-            like a GPS for your career journey.
-            It lists the optimal courses for you to take,
-            outlines the steps needed to achieve your goals,
-            and tracks your progress!
-            With our platform,
-            you'll have access to a tailored roadmap that empowers you to take control of your professional future.
-          </p>
-        </div>
-        <div className=" overflow-x-hidden mb-7 space-y-7">
+          <div className="flex flex-col items-center">
+            <div className="max-w-[94rem]">
+              <h1 className="font-bold text-xxl gradient-text bg-logo text-center mt-14">What is a Career Roadmap?</h1>
+              <p className="font-medium text-base text-center">
+                A career roadmap is your personalized guide to success,
+                like a GPS for your career journey.
+                It lists the optimal courses for you to take,
+                outlines the steps needed to achieve your goals,
+                and tracks your progress!
+                With our platform,
+                you'll have access to a tailored roadmap that empowers you to take control of your professional future.
+              </p>
+            </div>
+          </div>
           <h1 className="font-bold text-xxl gradient-text bg-logo text-center w-full">Your Path to Success</h1>
-          <div className="overflow-x-hidden flex justify-between space-x-4">
-            <div className="flex flex-col items-center border-2 border-purpleText rounded-md gap-7 p-5">
-              <img src={skillDev} alt="Man running" />
-              <h6 className="font-bold text-sm text-purpleText">Skill development roadmap</h6>
-              <p className="font-medium text-sm text-center">Receive a tailor-made roadmap outlining the skills you need to succeed in your chosen career path.</p>
-            </div>
-            <div className="flex flex-col items-center border-2 border-purpleText rounded-md gap-7 p-5">
-              <img src={progress} alt="Woman showing progress chart" />
-              <h6 className="font-bold text-sm text-purpleText">Progress Tracking and Analytics</h6>
-              <p className="font-medium text-sm text-center">Monitor your progress and track your accomplishments with detailed analytics and performance insights.</p>
-            </div>
-            <div className="flex flex-col items-center border-2 border-purpleText rounded-md gap-7 p-5">
-              <img src={career} alt="Man looking through binoculars at a city" />
-              <h6 className="font-bold text-sm text-purpleText">Career Path Exploration</h6>
-              <p className="font-medium text-sm text-center self-center">Explore various career paths aligned with your interests and skills with detailed insights and recommendations.</p>
-            </div>
+            <div className="flex justify-center gap-12">
+              <div className="flex flex-col items-center border-2 border-purpleText rounded-md gap-7 p-5 max-w-[24rem]">
+                <img src={skillDev} alt="Man running" />
+                <h6 className="font-bold text-sm text-purpleText">Skill development roadmap</h6>
+                <p className="font-medium text-sm text-center">Receive a tailor-made roadmap outlining the skills you need to succeed in your chosen career path.</p>
+              </div>
+              <div className="flex flex-col items-center border-2 border-purpleText rounded-md gap-7 p-5 max-w-[24rem]">
+                <img src={progress} alt="Woman showing progress chart" />
+                <h6 className="font-bold text-sm text-purpleText text-center">Progress Tracking and Analytics</h6>
+                <p className="font-medium text-sm text-center">Monitor your progress and track your accomplishments with detailed analytics and performance insights.</p>
+              </div>
+              <div className="flex flex-col items-center border-2 border-purpleText rounded-md gap-7 p-5 max-w-[24rem]">
+                <img src={career} alt="Man looking through binoculars at a city" />
+                <h6 className="font-bold text-sm text-purpleText">Career Path Exploration</h6>
+                <p className="font-medium text-sm text-center self-center">Explore various career paths aligned with your interests and skills with detailed insights and recommendations.</p>
+              </div>
           </div>
         </div>
       </div>
