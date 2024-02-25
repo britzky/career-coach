0f0993bbc9fca36d58cc82db1cfe274a574d5038
Britzky <johnclapper89@gmail.com>
2024-02-24 20:15:36 -0500

    Fixed landing page

--- frontend/career-coach/src/pages/LandingPage/LandingPage.tsx
+++ frontend/career-coach/src/pages/LandingPage/LandingPage.tsx
@@ -3,7 +3,7 @@ import { Footer } from "../../components";
 import skillDev from "../../assets/images/Moving forward.png"
 import progress from "../../assets/images/Progress overview.png"
 import career from "../../assets/images/Job hunt.png"
-import road from "../../assets/images/road.png"
+import road from "../../assets/images/road.svg"
 import { useCareerDetails } from "../../context/CareerContext";
 
 export const LandingPage = () => {
@@ -15,7 +15,7 @@ export const LandingPage = () => {
       <div className="mt-28 mx-10">
         <div className="flex flex-col mb-7 h-[695px]">
           <div className="relative mx-0 w-full">
-            <img src={road} alt="road" className="absolute -ml-28 mt-20 min-w-[2100px]" />
+            <img src={road} alt="road" className="absolute -ml-40 mt-20 min-w-[2750px]" />
           </div>
           <div className="max-w-[957px] flex flex-col">
             <span className="text-purpleText2 font-bold text-base">Get your DREAM JOB with our</span>
@@ -32,7 +32,7 @@ export const LandingPage = () => {
             </button>
           </div>
         </div>
-        <div className="overflow-x-hidden space-y-14 my-14 mt-56">
+        <div className="overflow-x-hidden space-y-14 my-14 mt-[54rem]">
           <h1 className="font-bold text-xxl gradient-text bg-logo text-center mt-14">What is a Career Roadmap?</h1>
           <p className="font-medium text-base">
             A career roadmap is your personalized guide to success,
