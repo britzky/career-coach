6d154bb52a541b2eec4be8db97d1e067097a854a
John <johnclapper89@gmail.com>
2024-02-28 16:24:24 -0500

    Fixed roadmap description size and month/overview card alignment

--- frontend/career-coach/src/pages/Roadmap/Summary.tsx
+++ frontend/career-coach/src/pages/Roadmap/Summary.tsx
@@ -4,7 +4,7 @@ export const Summary = () => {
     const { roadmap, careerDetails } = useCareerDetails()
 
   return (
-    <div className="-mt-4 w-full mb-10">
+    <div className="-mt-4 w-full mb-10 h-[210px] overflow-hidden">
         <h1 className="text-purpleText font-bold text-lg">{careerDetails.career} Roadmap ({careerDetails.experience})</h1>
         <p className="font-medium text-purpleText text-xs">{roadmap?.summary}</p>
     </div>
