eaf7269ca548f75d06e3d27bfd6f9f0074130f27
Britzky <johnclapper89@gmail.com>
2024-04-26 16:50:51 -0400

    Fixed blurred content's width

--- frontend/career-coach/src/pages/Roadmap/BlurredContent.tsx
+++ frontend/career-coach/src/pages/Roadmap/BlurredContent.tsx
@@ -3,13 +3,13 @@ import { DownArrow } from '../../assets/icons/DownArrow';
 
 export const BlurredContent = () => {
   return (
-    <div className="flex justify-center items-center">
-        <div className="flex flex-col items-center">
-            <div className="gradient-text bg-blurred-content font-semibold text-base mt-7">
+    <div className="flex justify-center items-center w-full">
+        <div className="flex flex-col items-center w-full">
+            <div className=" text-center gradient-text bg-blurred-content font-semibold text-base mt-7 w-full">
                 <h4>That's it for now! We are continuously working on our roadmap capabilities and will increase our ability to show more soon!</h4>
             </div>
-            <div className="flex flex-col items-center blur mt-10">
-                <div className="border-4 rounded-md border-purpleText p-5">
+            <div className="flex flex-col items-center blur mt-10 w-full">
+                <div className="border-4 rounded-md border-purpleText p-5 w-full">
                     <div className="flex flex-col">
                         <div className="flex justify-between">
                             <span className="font-bold text-base text-purpleText">Month 4: Lorem ipsum dolor sit amet consectetur adipisicing. </span>
@@ -27,7 +27,7 @@ export const BlurredContent = () => {
                 <div className="my-1">
                     <DownArrow />
                 </div>
-                <div className="border-4 rounded-md border-purpleText p-5">
+                <div className="border-4 rounded-md border-purpleText p-5 w-full">
                     <div className="flex flex-col">
                         <div className="flex justify-between">
                             <span className="font-bold text-base text-purpleText">Month 5: Lorem ipsum dolor sit amet consectetur adipisicing. </span>
@@ -45,7 +45,7 @@ export const BlurredContent = () => {
                 <div className="my-1">
                     <DownArrow />
                 </div>
-                <div className="border-4 rounded-md border-purpleText p-5">
+                <div className="border-4 rounded-md border-purpleText p-5 w-full">
                     <div className="flex flex-col">
                         <div className="flex justify-between">
                             <span className="font-bold text-base text-purpleText">Month 6: Lorem ipsum dolor sit amet consectetur adipisicing. </span>
