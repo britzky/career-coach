5504c5bca5fb32d546f3d9492a0079472896923a
Britzky <johnclapper89@gmail.com>
2024-06-13 12:09:36 -0400

    added margin between summary title and summary for roadmap

--- frontend/career-coach/src/pages/Roadmap/Summary.tsx
+++ frontend/career-coach/src/pages/Roadmap/Summary.tsx
@@ -5,7 +5,7 @@ export const Summary = () => {
 
   return (
     <div className="-mt-4 w-full mb-5 lg:h-[230px] overflow-hidden">
-        <h1 className="text-purpleText font-bold text-xs lg:text-lg leading-[20px] lg:leading-[50px]">{careerDetails.career} Roadmap ({careerDetails.experience})</h1>
+        <h1 className="text-purpleText font-bold text-xs lg:text-lg leading-[20px] lg:leading-[50px] mb-5">{careerDetails.career} Roadmap ({careerDetails.experience})</h1>
         <p className="font-medium text-purpleText text-xxxs lg:text-xs leading-[15px] lg:leading-[30px]">{roadmap?.summary}</p>
     </div>
   )
