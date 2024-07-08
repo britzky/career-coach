18b34f37343b4769160bf246ed3a86a63e537686
John <johnclapper89@gmail.com>
2024-07-08 18:48:21 -0400

    Set up for cloud deployment

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,12 +1,17 @@
+import os
 import openai
-from starlette.config import Config
 
-config = Config(".env")
+# Function to get environment variables with a default value
+def get_env_variable(var_name, default=None):
+    return os.getenv(var_name, default)
 
-openai_api_key = config('OPENAI_API_KEY', cast=str)
-assistant_id = config('OPEN_AI_ASSISTANT_ID', cast=str)
-database_url = config('DATABASE_URL', cast=str)
-SECRET_KEY = config('SECRET_KEY', cast=str)
-ALGORITHM = config('ALGORITHM', cast=str, default="HS256")
-ACCESS_TOKEN_EXPIRE_MINUTES = config("ACCESS_TOKEN_EXPIRE_MINUTES", cast=int, default=30)
+# Set up your configuration variables
+openai_api_key = get_env_variable('OPENAI_API_KEY')
+assistant_id = get_env_variable('OPEN_AI_ASSISTANT_ID')
+database_url = get_env_variable('DATABASE_URL')
+SECRET_KEY = get_env_variable('SECRET_KEY')
+ALGORITHM = get_env_variable('ALGORITHM', 'HS256')
+ACCESS_TOKEN_EXPIRE_MINUTES = int(get_env_variable('ACCESS_TOKEN_EXPIRE_MINUTES', '30'))
+
+# Initialize the OpenAI client
 client = openai.Client(api_key=openai_api_key)
