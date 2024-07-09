59c6e42868b3cf37173f93489374f1aceace50e3
John <johnclapper89@gmail.com>
2024-07-09 12:46:42 -0400

    Allowed all origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,16 +5,16 @@ from .routers import roadmap, quote
 # initialize the app
 app = FastAPI()
 
-allowed_origins = [
-    "http://localhost:5173",
-    "https://main--career-planner.netlify.app",
-    "https://career-planner.netlify.app"
+# allowed_origins = [
+#     "http://localhost:5173",
+#     "https://main--career-planner.netlify.app",
+#     "https://career-planner.netlify.app"
 
-]
+# ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allowed_origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers =["*"]
