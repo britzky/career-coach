f4db40602c79fded2ef03ffaa7c6091326020f8d
John <johnclapper89@gmail.com>
2024-07-12 14:28:24 -0400

    Fixed typo

---
+++ backend/tests/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+from fastapi.testclient import TestClient
+from app.main import app
+
+@pytest.fixture(scope="module")
+def client():
+    with TestClient(app) as client:
+        yield client
